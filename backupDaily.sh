62d41974e76e8560adea5403b5029ad3e15e5c5b
fontoura <v.abreu.fontoura@gmail.com>
2022-06-21 23:32:06 -0300

    paths and variables changed to .env

--- backupDaily.sh
+++ backupDaily.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-
-USER=nickste
+source .env
 
 #data para nomear arquivo.tar.gz
 DATE=`date "+%d-%m-%Y-%H:%M"`
 
-#ID do arquivo no Drive
-DRIVEFILE="1VHnDVXeG31F6RQP_ew_tIWEmJlFQg3-8"
 #Arquivos a fazer backup
-BKPDIRS=(/home/${USER}/.ssh/ /home/${USER}/backupDir/ /home/${USER}/.local/share/Trash)
+echo "backing up these dirs: ${BKPDIRS[@]}"
+
 #Número de arquivos no Drive
-NBKPSDRIVE=`/home/${USER}/Desktop/./gdrive sync content --order createdTime ${DRIVEFILE} | wc -l`
+NBKPSDRIVE=`${GDRIVEPATH} sync content --order createdTime ${DRIVEFILE} | wc -l`
+echo "Number of files on Drive dir: ${NBKPSDRIVE}"
+
 #Se a quantidade de arquivos salvos no Drive for maior que 5
 if [ ${NBKPSDRIVE} -ge 5 ]
 then
@@ -19,21 +19,26 @@ then
 	LINE=$((NBKPSDRIVE - 4))
 
 	#Encontra o arquivo.tar.gz mais antigo no Drive
-	OLDESTBKPDRIVE=`/home/$USER/Desktop/./gdrive sync content --order createdTime ${DRIVEFILE} | sed "${LINE}q;d" | awk '{print $1}'`
-	NBKPSPC=`ls /var/backups/daily/ | wc -l`
+	OLDESTBKPDRIVE=`${GDRIVEPATH} sync content --order createdTime ${DRIVEFILE} | sed "${LINE}q;d" | awk '{print $1}'`
+
+	NBKPSPC=`ls ${BKPPATH} | wc -l`
+	echo "Number of backups on local: ${NBKPSPC}"
 	#Se a quantidade de backups for >= 5
 	if [ ${NBKPSPC} -ge 5 ]
 	then
 		#remove esse mesmo arquivo mais antigo da máquina
-		sudo rm /var/backups/daily/"$(ls -t /var/backups/daily | tail -1)"
+		echo "removing $(ls -t /var/backups/daily | tail -1) from local"
+		rm ${BKPPATH}"$(ls -t /var/backups/daily | tail -1)"
 	fi
 	#remove o arquivo mais antigo do Drive
-	/home/${USER}/Desktop/./gdrive delete ${OLDESTBKPDRIVE}
+	${GDRIVEPATH} delete ${OLDESTBKPDRIVE}
+	echo "Removing ${OLDESTBKPDRIVE} from drive"
 fi
 
 sleep 1
 #faz o backup dos diretórios escolhidos
-sudo tar -czf /var/backups/daily/${DATE}.tar.gz ${BKPDIRS[@]}
+tar -czf ${BKPPATH}${DATE}.tar.gz ${BKPDIRS[@]}
+
 #envia o backup feito para o Drive
-/home/${USER}/Desktop/./gdrive sync upload /var/backups/daily/ ${DRIVEFILE}
+${GDRIVEPATH} sync upload ${BKPPATH} ${DRIVEFILE}
 
